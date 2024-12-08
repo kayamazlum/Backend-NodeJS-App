db0f22540cb609d8b139419fa97f1ba7af18abf3
kayamazlum <mazlum09kaya@gmail.com>
2024-12-08 20:40:34 +0300

    role icin crud islemelri yapildi

--- api/app.js
+++ api/app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  console.log("Bu bir middleware denemesidir.....");
+  console.log("Bu bir middleware dir.....");
   next();
 });
 
