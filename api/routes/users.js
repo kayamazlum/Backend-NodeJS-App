c3b737956cb8bb5e423a315795c3109eea4d75ec
kayamazlum <mazlum09kaya@gmail.com>
2024-12-10 19:27:28 +0300

    users endpointlerine basladım

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,26 @@
-var express = require('express');
+var express = require("express");
+const Users = require("../db/models/Users");
+const Response = require("../lib/Response");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", async (req, res, next) => {
+  try {
+    let users = await Users.find({});
+
+    res.json(Response.successResponse(users));
+  } catch (err) {
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+});
+
+router.post("/add", async (req, res) => {
+  try {
+  } catch (err) {
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(errorResponse);
+  }
 });
 
 module.exports = router;
