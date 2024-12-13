451c237cdf0a67e801199c4548f16ddbd9540594
kayamazlum <mazlum09kaya@gmail.com>
2024-12-13 22:03:45 +0300

    audit log yapildi

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -4,6 +4,7 @@ const Categories = require("../db/models/Categories");
 const Response = require("../lib/Response");
 const CustomError = require("../lib/Error");
 const Enum = require("../config/Enum");
+const AuditLogs = require("../lib/AuditLogs");
 
 router.get("/", async (req, res, next) => {
   try {
@@ -33,6 +34,8 @@ router.post("/add", async (req, res) => {
 
     await category.save();
 
+    AuditLogs.info(req.user?.email, "Categories", "Add", category);
+
     res.json(Response.successResponse({ success: true }));
   } catch (err) {
     let errorResponse = Response.errorResponse(err);
@@ -56,6 +59,12 @@ router.post("/update", async (req, res) => {
     if (typeof body.is_active === "boolean") updates.is_active = body.is_active;
 
     await Categories.updateOne({ _id: body._id }, updates);
+
+    AuditLogs.info(req.user?.email, "Categories", "Update", {
+      _id: body._id,
+      ...updates,
+    });
+
     res.json(Response.successResponse({ success: true }));
   } catch (err) {
     let errorResponse = Response.errorResponse(err);
@@ -75,6 +84,10 @@ router.post("/delete", async (req, res) => {
     }
 
     await Categories.deleteOne({ _id: body._id });
+
+    AuditLogs.info(req.user?.email, "Categories", "Delete", {
+      _id: body._id,
+    });
 
     res.json(Response.successResponse({ success: true }));
   } catch (err) {
